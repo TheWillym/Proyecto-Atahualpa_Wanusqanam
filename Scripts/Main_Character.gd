34cb79287ad58f7b549d3177d249eaf46a126c51
TheWillym <100740487+TheWillym@users.noreply.github.com>
2023-10-17 11:12:46 -0500

    Init

--- Scripts/Main_Character.gd
+++ Scripts/Main_Character.gd
@@ -1,28 +1,41 @@
 extends CharacterBody2D
 
+@export var moveSpeed := 25
+@export var maxSpeed := 50
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var jumpHeight := - 150
+@export var maxJump := 1
+@export var numJump := 0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+@onready var sprite = $Sprite2D
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func _process(delta):
+	movePJ()
+	jump()
+	
+func jump():
+	if is_on_floor() && numJump != 0:
+		numJump = 0
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if numJump < maxJump:
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = jumpHeight
+			numJump += 1
+				
+func movePJ():
+	if Input.is_action_pressed("Right"):
+		sprite.flip_h= false
+		velocity.x = min(velocity.x+moveSpeed,maxSpeed)
+	elif Input.is_action_pressed("Left"):
+		sprite.flip_h= true
+		velocity.x = max(velocity.x-moveSpeed,-maxSpeed)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, maxSpeed)
 
 	move_and_slide()
