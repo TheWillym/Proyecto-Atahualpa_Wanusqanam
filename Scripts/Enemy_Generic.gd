53a1e2cec983667cbf2824282e3b16c9084c4e7a
TheWillym <100740487+TheWillym@users.noreply.github.com>
2023-10-24 10:59:46 -0500

    Add

--- Scripts/Enemy_Generic.gd
+++ Scripts/Enemy_Generic.gd
@@ -1,3 +1,22 @@
 extends CharacterBody2D
+class_name Enemy
 
-pass
+#Variable de movimiento horizontal.
+@export var move_Speed = 60
+
+#Variable de gravedad.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var sensor: Area2D = $"Sensor"
+
+var Main_Character: CollisionObject2D
+
+	#PROCESOS
+
+#Proceso para la gravedad.
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+func _on_sensor_body_entered(body):
+	pass # Replace with function body.
